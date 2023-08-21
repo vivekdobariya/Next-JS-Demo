32338164878ba358ac4253bd1a137e6f796de3cd
vivekdobariya <vivekdobariya333@gmail.com>
2023-08-21 22:15:12 +0530

    first commite

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+// import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+    </html> 
   )
 }
